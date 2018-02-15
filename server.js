48c54857c27fa54db6a07155db174a78fce7c81f
Pruthviraj Patil <pruthvirajr.patil2016@vitstudent.ac.in>
2018-02-15 18:01:25 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,8 +58,8 @@ function createTemplate(data)
 };
 
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 
