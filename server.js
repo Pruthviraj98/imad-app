ffc0f5db986847df34045ee1e722d6c57581b797
Pruthviraj Patil <pruthvirajr.patil2016@vitstudent.ac.in>
2018-02-15 16:55:38 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,17 +9,37 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var article-one={
+var articleOne={
   title:'article-one',
   date:'15 feb 2018',
-  content:'<html> </html>'
+  list:'<ul> <li>C</li> <li>CPP</li> <li>Javascript</li> <li>PHP</li><li>python</li> </ui>',
+  content:'<h1>I am very Enthusiastic in Techstuffs. Imad attracted me as I got Interested for the mobile application development.</h1>'
 };
 
+function createTemplate(data)
+{
+    title=data.title;
+    list=data.list;
+    date=data.date;
+    var htmlTemplate=
+        `<!doctype html>
+        <html>
+        <title>${title}</title>
+        <head>This is the article-one</head>
+        <h2>${date}</h2>
+        <body>
+        <h1>This is the list of Programming languages that I know</h1>
+        ${list}
+        <br>
+        <br>
+        <div class="second" style="background-color:black; color:white">
+        ${content}
+        </div>
+        </body>
+        </html>`;  
 
-
-
-
-
+    return htmlTemplate;
+};
 
 
 app.get('/ui/style.css', function (req, res) {
@@ -28,7 +48,7 @@ app.get('/ui/style.css', function (req, res) {
 
 
 app.get('/article-one', function( req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function( req, res){
