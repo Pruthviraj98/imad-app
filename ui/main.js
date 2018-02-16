1ba8f4e81042ecff590679d1f836a8d2b0043b7f
Pruthviraj Patil <pruthvirajr.patil2016@vitstudent.ac.in>
2018-02-16 18:14:14 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 
 //change the text of main-text div
 
 //var  element=document.getElementById("main-text");
 
 //element.innerHTML="newvalue";
-
+/*
 var img = document.getElementById("madi");
 
 var marginLeft=0;
@@ -17,4 +17,19 @@ function moveRight(){
 
 img.onclick=function(){
     var interval=setInterval(moveRight, 100);
-};
+};*/
+
+var element=document.getElementById("counter");
+var counter=0;
+
+
+element.onclick=function(){
+//make a request to the counter end point;
+counter=counter+1
+
+//capture the response and store it in a variable
+var span=document.getElementById("count");
+
+//render the incremented counter value to the spanned area.
+span.innerHTML=counter.toString();
+}
