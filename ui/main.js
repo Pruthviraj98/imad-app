0584a0d59d232167252625e776f6cea7465840a3
Pruthviraj Patil <pruthvirajr.patil2016@vitstudent.ac.in>
2018-02-16 17:42:10 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,9 @@ console.log('Loaded!');
 
 //change the text of main-text div
 
-var  element=document.getElementById("main-text");
+//var  element=document.getElementById("main-text");
 
-element.innerHTML="newvalue";
+//element.innerHTML="newvalue";
 
 var img = document.getElementById("madi");
 
