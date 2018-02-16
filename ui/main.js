04fabce059ac271b7ea881cf50e55f6b643bfd51
Pruthviraj Patil <pruthvirajr.patil2016@vitstudent.ac.in>
2018-02-16 19:04:25 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,10 +30,10 @@ button.onclick=function()
     //capture the request and stare it in the variable
     request.onreadystatechange=function()
     {
-        if(request.readyState==XMLHttpResponse.DONE)
+        if(request.readyState===XMLHttpRequest.DONE)
         {
             //take some action
-            if(request.status==200)
+            if(request.status===200)
             {
                 var counter=request.responseText;
                 var span=document.getElementById('count');
