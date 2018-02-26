40cb05d9bb0b39cf81a27e52a3da3af8d9509f7b
Pruthviraj Patil <pruthvirajr.patil2016@vitstudent.ac.in>
2018-02-26 17:17:45 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.get('/articles/:articleName', function( req, res)
     //articles[articleName]=={}content object for article one
     var articleName=req.params.articleName;
     
-    pool.query("Select * from article where title='"+req.params.articleName+"'", function(err, result)
+    pool.query("Select * from article where title= $1", [req.params.articleName], function(err, result)
     {
         if(err)
         {
