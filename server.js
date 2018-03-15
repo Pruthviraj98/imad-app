8fc365b94a6cc7702bfe1d28d425023f120f2acd
Pruthviraj Patil <pruthvirajr.patil2016@vitstudent.ac.in>
2018-03-15 21:49:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');//to help us output logs
 var path = require('path');
 var crypto=require('crypto');
 var Pool=require('pg').Pool;
-var bodyParser=request('body-parser');
+var bodyParser=require('body-parser');
 
 var config={
     user:'pruthvirajrpatil2016',
